01baffcfbf311ab55d202c2479f6bb75372ed0b1
bacaiib <roman-frommhold@gmx.net>
2024-05-13 20:19:47 +0200

    datanbank2

---
+++ datenB/WinArtikelNeu.java
@@ -0,0 +1,86 @@
+package datenB;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class WinArtikelNeu extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	JLabel lblNr;
+	JTextField tfNr;
+	JLabel lblBezeichnung;
+	JTextField tfBezeichnung;
+	JLabel lblPreis;
+	JTextField tfPreis;
+	JButton btnSpeichern;
+
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public WinArtikelNeu() {
+		setTitle("Neuer Artikel");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE); // Nur dieses Sub-Window schließen
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblNr = new JLabel("Nr");
+		lblNr.setBounds(10, 28, 46, 14);
+		contentPane.add(lblNr);
+		
+		tfNr = new JTextField();
+		tfNr.setBounds(96, 25, 46, 20);
+		contentPane.add(tfNr);
+		tfNr.setColumns(10);
+		
+		lblBezeichnung = new JLabel("Bezeichnung");
+		lblBezeichnung.setBounds(10, 67, 71, 14);
+		contentPane.add(lblBezeichnung);
+		
+		tfBezeichnung = new JTextField();
+		tfBezeichnung.setBounds(96, 64, 177, 20);
+		contentPane.add(tfBezeichnung);
+		tfBezeichnung.setColumns(10);
+		
+		lblPreis = new JLabel("Preis");
+		lblPreis.setBounds(10, 108, 71, 14);
+		contentPane.add(lblPreis);
+		
+		tfPreis = new JTextField();
+		tfPreis.setBounds(96, 105, 94, 20);
+		contentPane.add(tfPreis);
+		tfPreis.setColumns(10);
+		
+		btnSpeichern = new JButton("SPEICHERN");
+		btnSpeichern.addActionListener(this);
+		btnSpeichern.setBounds(96, 146, 130, 23);
+		contentPane.add(btnSpeichern);
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnSpeichern) {
+			do_btnSpeichern_actionPerformed(e);
+		}
+	}
+	protected void do_btnSpeichern_actionPerformed(ActionEvent e) {
+		Artikel artikel = new Artikel();
+		artikel.setNr( Integer.parseInt( tfNr.getText() ) );
+		artikel.setBezeichnung( tfBezeichnung.getText() );
+		artikel.setPreis( Double.parseDouble( tfPreis.getText() ) );
+		
+		Datenbank.artikelSpeichern(artikel);
+	}
+}
